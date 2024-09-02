1e950fffc8cbb22fc645bf74800e3497753bfbf3
Damilare1212 <damilarelamina@gmail.com>
2024-09-02 07:01:53 +0100

    Add project files.

---
+++ Online BookStore/OnlineLibrary/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace OnlineLibrary.Data
+{
+    public class ApplicationDbContext : IdentityDbContext 
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+          public DbSet<OnlineLibrary.Models.Book> Book { get; set; }
+        public DbSet<OnlineLibrary.Models.Author> Author { get; set; }
+        public DbSet<OnlineLibrary.Models.Genre> Genre { get; set; }
+    }
+}
---
+++ Online BookStore/README.md
@@ -0,0 +1,3 @@
+ 
+An online book management system, built with ASP.NET MVC in C#
+Built for Line of Hearts Technology
